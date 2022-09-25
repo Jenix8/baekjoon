c9616eab975542a55762a983d61592a6a130de2e
Jenix82 <cjlhs0691@naver.com>
2022-09-26 01:12:10 +0900

    220926

---
+++ 단계별로 풀어보기/기하/1358/1358/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int W, H, X, Y, P;
+	cin >> W >> H >> X >> Y >> P;
+	int count = 0;
+	int x, y;
+	for (int i = 0; i < P; i++) {
+		cin >> x >> y;
+		
+		if (x - X >= 0 && x - X <= W) {
+			if (y - Y >= 0 && y - Y <= H) {
+				count++;
+				continue;
+			}
+		}
+
+		if (pow(x - X, 2) + pow(y - (Y + H / 2), 2) <= 0.25 * H * H) {
+			count++;
+			continue;
+		}
+
+		if (pow(x - (X + W), 2) + pow(y - (Y + H / 2), 2) <= 0.25 * H * H) {
+			count++;
+			continue;
+		}
+	}
+	cout << count << endl;
+
+	return 0;
+}
